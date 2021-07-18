2eea02c6df8c2ee2016827dc4651b0005dbcf93a
linzihong <517359919@qq.com>
2021-07-18 12:46:17 +0800

    init

---
+++ src/main/java/net/coding/geekquestionnaire/GeekQuestionnaireApplication.java
@@ -0,0 +1,13 @@
+package net.coding.geekquestionnaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekQuestionnaireApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GeekQuestionnaireApplication.class, args);
+  }
+
+}
